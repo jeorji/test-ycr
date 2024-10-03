57ef6d1cb3fa73b01c155a50b574275bdca905ba
jeorji <118723195+jeorji@users.noreply.github.com>
2024-10-03 20:32:09 +0700

    Create echo_server.py

---
+++ echo_server.py
@@ -0,0 +1,24 @@
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_address = ('0.0.0.0', 8080)
+server_socket.bind(server_address)
+
+server_socket.listen(1)
+print("Echo server is running on port 8080...")
+
+while True:
+    connection, client_address = server_socket.accept()
+    try:
+        print(f"Connection from {client_address}")
+        
+        while True:
+            data = connection.recv(1024)
+            if data:
+                print(f"Received: {data}")
+                connection.sendall(data)
+            else:
+                break
+    finally:
+        connection.close()
